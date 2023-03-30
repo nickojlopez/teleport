8221c8f31787bbdcd4451bac9da6d9a997392f3d
Andrew LeFevre <andrew.lefevre@goteleport.com>
2023-03-30 17:52:17 -0400

    refactor sftp.ParseDestination

--- lib/sshutils/sftp/parse.go
+++ lib/sshutils/sftp/parse.go
@@ -17,38 +17,18 @@ limitations under the License.
 package sftp
 
 import (
-	"regexp"
+	"strings"
 
 	"github.com/gravitational/teleport/lib/utils"
 	"github.com/gravitational/trace"
 )
 
-var reSFTP = regexp.MustCompile(
-	// optional username, note that outside group
-	// is a non-capturing as it includes @ signs we don't want
-	`(?:(?P<username>.+)@)?` +
-		// either some stuff in brackets - [ipv6]
-		// or some stuff without brackets and colons
-		`(?P<host>` +
-		// this says: [stuff in brackets that is not brackets] - loose definition of the IP address
-		`(?:\[[^@\[\]]+\])` +
-		// or
-		`|` +
-		// some stuff without brackets or colons to make sure the OR condition
-		// is not ambiguous
-		`(?:[^@\[\:\]]+)` +
-		`)` +
-		// after colon, there is a path that could consist technically of
-		// any char including empty which stands for the implicit home directory
-		`:(?P<path>.*)`,
-)
-
-// Destination is SCP destination to copy to or from
+// Destination is a SFTP destination to copy to or from
 type Destination struct {
 	// Login is an optional login username
 	Login string
 	// Host is a host to copy to/from
-	Host utils.NetAddr
+	Host *utils.NetAddr
 	// Path is a path to copy to/from.
 	// An empty path name is valid, and it refers to the user's default directory (usually
 	// the user's home directory).
@@ -56,24 +36,82 @@ type Destination struct {
 	Path string
 }
 
-// ParseSCPDestination takes a string representing a remote resource for SFTP
-// to download/upload, like "user@host:/path/to/resource.txt" and parses it into
+// ParseDestination takes a string representing a remote resource for SFTP
+// to download/upload in the form "[user@]host:[path]" and parses it into
 // a structured form.
 //
 // See https://tools.ietf.org/html/draft-ietf-secsh-filexfer-09#page-14, 'File Names'
 // section about details on file names.
-func ParseDestination(s string) (*Destination, error) {
-	out := reSFTP.FindStringSubmatch(s)
-	if len(out) < 4 {
-		return nil, trace.BadParameter("failed to parse %q, try form user@host:/path", s)
+func ParseDestination(input string) (*Destination, error) {
+	firstColonIdx := strings.Index(input, ":")
+	if firstColonIdx == -1 {
+		return nil, trace.BadParameter("%q is missing a path, use form [user@]host:[path]", input)
 	}
-	addr, err := utils.ParseAddr(out[2])
+	loginEndIdx := strings.LastIndex(input, "@")
+	// if there is no input after the last '@', then host and path are missing
+	if len(input) == loginEndIdx+1 {
+		return nil, trace.BadParameter("%q is missing a host and path, use form [user@]host:[path]", input)
+	}
+
+	var login string
+	// If at least one '@' exists and is before the first ':', get the
+	// login. Otherwise, either there are no '@' or all '@' are after
+	// the first ':' (where the host or path starts), so no login was
+	// specified.
+	if loginEndIdx != -1 && loginEndIdx < firstColonIdx {
+		login = input[:loginEndIdx]
+		loginEndIdx++
+	} else {
+		loginEndIdx = 0
+	}
+
+	hostEnd := firstColonIdx + 1
+	host, err := utils.ParseAddr(input[loginEndIdx:firstColonIdx])
 	if err != nil {
-		return nil, trace.Wrap(err)
+		// parsing the input after the login failed, so the host is
+		// invalid unless the host is an IPv6 address, as then the
+		// host won't end at the first colon
+
+		// if there is only one ':' in the entire input, the host isn't
+		// an IPv6 address
+		if strings.Count(input, ":") == 1 {
+			return nil, trace.Wrap(err)
+		}
+
+		// an IPv6 address here must be enclosed by braces, if the host
+		// isn't this isn't an IPv6 address
+		afterLogin := input[loginEndIdx:]
+		if afterLogin[0] != '[' {
+			return nil, trace.Wrap(err)
+		}
+		rbraceIdx := strings.Index(afterLogin, "]")
+		if rbraceIdx == -1 {
+			return nil, trace.Wrap(err)
+		}
+		// if there's nothing after ']' then the path is missing
+		if len(afterLogin) <= rbraceIdx+2 {
+			return nil, trace.Wrap(err)
+		}
+
+		maybeAddr := afterLogin[:rbraceIdx+1]
+		host, err = utils.ParseAddr(maybeAddr)
+		if err != nil {
+			return nil, trace.Wrap(err)
+		}
+		// the host ends after the login + the IPv6 address
+		// (including the trailing ']') and a ':'
+		hostEnd = loginEndIdx + rbraceIdx + 2
 	}
-	path := out[3]
-	if path == "" {
-		path = "."
+
+	// if there is nothing after the host the path defaults to "."
+	path := "."
+	if len(input) > hostEnd {
+		path = input[hostEnd:]
 	}
-	return &Destination{Login: out[1], Host: *addr, Path: path}, nil
+
+	return &Destination{
+		Login: login,
+		Host:  host,
+		Path:  path,
+	}, nil
 }
--- lib/sshutils/sftp/parse_test.go
+++ lib/sshutils/sftp/parse_test.go
@@ -25,61 +25,153 @@ import (
 	"github.com/gravitational/teleport/lib/utils"
 )
 
-func TestDestinationParsing(t *testing.T) {
+var testCases = []struct {
+	comment string
+	in      string
+	dest    Destination
+	err     string
+}{
+	{
+		comment: "full spec of the remote destination",
+		in:      "root@remote.host:/etc/nginx.conf",
+		dest: Destination{
+			Login: "root",
+			Host: &utils.NetAddr{
+				Addr:        "remote.host",
+				AddrNetwork: "tcp",
+			},
+			Path: "/etc/nginx.conf",
+		},
+	},
+	{
+		comment: "spec with just the remote host",
+		in:      "remote.host:/etc/nginx.co:nf",
+		dest: Destination{
+			Host: &utils.NetAddr{
+				Addr:        "remote.host",
+				AddrNetwork: "tcp",
+			},
+			Path: "/etc/nginx.co:nf",
+		},
+	},
+	{
+		comment: "ipv6 remote destination address",
+		in:      "[::1]:/etc/nginx.co:nf",
+		dest: Destination{
+			Host: &utils.NetAddr{
+				Addr:        "[::1]",
+				AddrNetwork: "tcp",
+			},
+			Path: "/etc/nginx.co:nf",
+		},
+	},
+	{
+		comment: "full spec of the remote destination using ipv4 address",
+		in:      "root@123.123.123.123:/var/www/html/",
+		dest: Destination{
+			Login: "root",
+			Host: &utils.NetAddr{
+				Addr:        "123.123.123.123",
+				AddrNetwork: "tcp",
+			},
+			Path: "/var/www/html/",
+		},
+	},
+	{
+		comment: "target location using wildcard",
+		in:      "myusername@myremotehost.com:/home/hope/*",
+		dest: Destination{
+			Login: "myusername",
+			Host: &utils.NetAddr{
+				Addr:        "myremotehost.com",
+				AddrNetwork: "tcp",
+			},
+			Path: "/home/hope/*",
+		},
+	},
+	{
+		comment: "complex login",
+		in:      "complex@example.com@remote.com:/anything.txt",
+		dest: Destination{
+			Login: "complex@example.com",
+			Host: &utils.NetAddr{
+				Addr:        "remote.com",
+				AddrNetwork: "tcp",
+			},
+			Path: "/anything.txt",
+		},
+	},
+	{
+		comment: "implicit user's home directory",
+		in:      "root@remote.host:",
+		dest: Destination{
+			Login: "root",
+			Host: &utils.NetAddr{
+				Addr:        "remote.host",
+				AddrNetwork: "tcp",
+			},
+			Path: ".",
+		},
+	},
+	{
+		comment: "no login and '@' in path",
+		in:      "remote.host:/some@file",
+		dest: Destination{
+			Host: &utils.NetAddr{
+				Addr:        "remote.host",
+				AddrNetwork: "tcp",
+			},
+			Path: "/some@file",
+		},
+	},
+	{
+		comment: "no login, '@' and ':' in path",
+		in:      "remote.host:/some@remote:file",
+		dest: Destination{
+			Host: &utils.NetAddr{
+				Addr:        "remote.host",
+				AddrNetwork: "tcp",
+			},
+			Path: "/some@remote:file",
+		},
+	},
+	{
+		comment: "complex login, IPv6 addr and ':' in path",
+		in:      "complex@user@[::1]:/remote:file",
+		dest: Destination{
+			Login: "complex@user",
+			Host: &utils.NetAddr{
+				Addr:        "[::1]",
+				AddrNetwork: "tcp",
+			},
+			Path: "/remote:file",
+		},
+	},
+}
+
+func TestParseDestination(t *testing.T) {
 	t.Parallel()
 
-	testCases := []struct {
-		comment string
-		in      string
-		dest    Destination
-		err     error
-	}{
-		{
-			comment: "full spec of the remote destination",
-			in:      "root@remote.host:/etc/nginx.conf",
-			dest:    Destination{Login: "root", Host: utils.NetAddr{Addr: "remote.host", AddrNetwork: "tcp"}, Path: "/etc/nginx.conf"},
-		},
-		{
-			comment: "spec with just the remote host",
-			in:      "remote.host:/etc/nginx.co:nf",
-			dest:    Destination{Host: utils.NetAddr{Addr: "remote.host", AddrNetwork: "tcp"}, Path: "/etc/nginx.co:nf"},
-		},
-		{
-			comment: "ipv6 remote destination address",
-			in:      "[::1]:/etc/nginx.co:nf",
-			dest:    Destination{Host: utils.NetAddr{Addr: "[::1]", AddrNetwork: "tcp"}, Path: "/etc/nginx.co:nf"},
-		},
-		{
-			comment: "full spec of the remote destination using ipv4 address",
-			in:      "root@123.123.123.123:/var/www/html/",
-			dest:    Destination{Login: "root", Host: utils.NetAddr{Addr: "123.123.123.123", AddrNetwork: "tcp"}, Path: "/var/www/html/"},
-		},
-		{
-			comment: "target location using wildcard",
-			in:      "myusername@myremotehost.com:/home/hope/*",
-			dest:    Destination{Login: "myusername", Host: utils.NetAddr{Addr: "myremotehost.com", AddrNetwork: "tcp"}, Path: "/home/hope/*"},
-		},
-		{
-			comment: "complex login",
-			in:      "complex@example.com@remote.com:/anything.txt",
-			dest:    Destination{Login: "complex@example.com", Host: utils.NetAddr{Addr: "remote.com", AddrNetwork: "tcp"}, Path: "/anything.txt"},
-		},
-		{
-			comment: "implicit user's home directory",
-			in:      "root@remote.host:",
-			dest:    Destination{Login: "root", Host: utils.NetAddr{Addr: "remote.host", AddrNetwork: "tcp"}, Path: "."},
-		},
-	}
 	for _, tt := range testCases {
 		tt := tt
 		t.Run(tt.comment, func(t *testing.T) {
 			resp, err := ParseDestination(tt.in)
-			if tt.err != nil {
-				require.IsType(t, err, tt.err)
+			if tt.err != "" {
+				require.EqualError(t, err, tt.err)
 				return
 			}
 			require.NoError(t, err)
 			require.Empty(t, cmp.Diff(resp, &tt.dest))
 		})
 	}
+}
+
+func FuzzParseDestination(f *testing.F) {
+	for _, tt := range testCases {
+		f.Add(tt.in)
+	}
+
+	f.Fuzz(func(t *testing.T, input string) {
+		_, _ = ParseDestination(input)
+	})
 }
